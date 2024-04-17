d4164a07dc381ac03a73bcb2cee6c15b3aa04b40
George <georgesmith0215@gmail.com>
2024-04-17 17:13:18 +0800

    feat: layout && interceptor && BeforeEach

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,5 +1,8 @@
 import axios from 'axios'
 import type { AxiosRequestConfig, AxiosResponse } from 'axios'
+import store from '../store';
+import { message } from 'antd';
+import { clearToken } from '../store/modules/users';
 
 const instance = axios.create({
   baseURL: 'http://localhost:3000/',
@@ -7,12 +10,22 @@ const instance = axios.create({
 });
 
 instance.interceptors.request.use(function (config) {
+  if(config.headers){
+    config.headers.authorization = store.getState().users.token;
+  }
   return config;
 }, function (error) {
   return Promise.reject(error);
 });
 
 instance.interceptors.response.use(function (response) {
+  if(response.data.errmsg === 'token error'){
+    message.error('token error')
+    store.dispatch(clearToken())
+    setTimeout(()=>{
+      window.location.replace('/login')
+    }, 1000)
+  }
   return response;
 }, function (error) {
   return Promise.reject(error);
---
+++ src/views/Home/components/HomeBreadcrumb.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from '../Home.module.scss'
+import { Breadcrumb } from 'antd'
+import { useLocation, matchRoutes } from 'react-router-dom'
+import { routes } from '../../../router'
+
+export default function HomeBreadcrumb() {
+  const location = useLocation()
+  const matchs = matchRoutes(routes, location)
+  console.log(matchs)
+  return (
+    <Breadcrumb className={styles['home-breadcrumb']}>
+      { matchs?.map((v)=> <Breadcrumb.Item key={v.pathnameBase}>{v.route.meta?.title}</Breadcrumb.Item>) }
+    </Breadcrumb>
+  )
+}
